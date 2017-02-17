e1842e572a9d2e11946b6ef1cc05520f11795061
Jim Read <jimmydean886@hotmail.com>
2017-02-17 01:00:40 -0500

    Tailored the code to meet specific requirements
    Tailored the parser code to meet the requirements of this project. This
    program will now correctly parse quiz files into memory. It does not,
    however, output them yet.

--- cxqweb-source-main.cc
+++ cxqweb-source-main.cc
@@ -3,36 +3,45 @@
 #include"listhelper/listhelper.cc"
 using namespace std;
 
-struct question{
+struct option{
 	string identifier;
 	string description;
+};
+
+struct question{
+	string description;
+	singlelinked<option> optionlist;
 };
 
 struct quiz{
 	string name;
 	singlelinked<question> questionlist;
-};
+} master_quiz;
 
+/* return codes:
+1: invalid [quiz] level key/value pair
+2: invalid [question] level key/value pair
+3: invalid [] section
+4: file open issue
+*/
 int getQuizResults(string filename){
 	ifstream quizfile;
 	quizfile.open(filename.c_str());
 	if(!quizfile.is_open()){
-		cout<<"<h1>Bad quiz ID.</h1>"<<endl;
-		cout<<"<p>The quiz ID you have supplied is wrong. Please ask your instructor for a new one.</p>"<<endl;
-		cout<<"</body>"<<endl<<"</html>"<<endl;
-		return 2;
+		return 4;
 	}
 	enum level{
 		quizlevel,
 		questionlevel
-	};
+	} currentlevel;
 	string key;
 	string value;
 	string currentline;
 	int spaces;
-	level currentlevel;
 	currentlevel = quizlevel;
 	string values[4]; //common values label.
+	question currentquestion;
+	option currentoption;
 	while(!quizfile.fail()){
 		getline(quizfile, currentline);
 		spaces = 0;
@@ -54,21 +63,32 @@ int getQuizResults(string filename){
 		}
 		if(key[0] == '['){
 			if(currentlevel == quizlevel){
-				//I don't think we need anything on the quiz level
+				//TODO: figure out if quiz-level logic is needed
 			} else if (currentlevel == questionlevel){
-				//do question-level stuff
+				master_quiz.questionlist.push_back(currentquestion);
 			}
+			if(key == "[quiz]") currentlevel = quizlevel;
+			else if(key == "[question]") currentlevel = questionlevel;
+			else return 3;
 		}
 		else{
 			if(currentlevel == quizlevel){
-				//do key/value parsing
+				if(key == "name") master_quiz.name = value;
+				else return 1;
 			}
 			if(currentlevel == questionlevel){
-				//do question level stuff
+				if(key != "description"){
+					currentoption.identifier = key;
+					currentoption.description = value;
+					currentquestion.optionlist.push_back(currentoption);
+				} else if (key == "description") {
+					currentquestion.description = value;
+				}
+				else return 2;
 			}
 		}
 	}
-	//repeat key/value parsing code here
+	master_quiz.questionlist.push_back(currentquestion);
 	quizfile.close();
 }
 
@@ -76,7 +96,7 @@ int getQuizResults(string filename){
 Return codes:
 0 - success
 1 - no test id
-2 - test ID not found
+2 - problem parsing quiz file (io, syntax, etc.)
 */
 int main(int argc, char* argv[]){
 	bool good = true;
@@ -93,12 +113,32 @@ int main(int argc, char* argv[]){
 		cout<<"<h1>Take a Quiz</h1>"<<endl;
 	}
 	else{
-		cout<<"<h1>Bad quiz ID.</h1>"<<endl;
-		cout<<"<p>The quiz ID you have supplied is wrong. Please ask your instructor for a new one.</p>"<<endl;
-		cout<<"</body>"<<endl<<"</html>"<<endl;
+		cout<<"<h1>No quiz ID.</h1>"<<endl;
+		cout<<"<p>You have not supplied an appropriate quiz id. Please ask your instructor for a new link.</p>"<<endl;
 		return 1;
 	}
 	string name = argv[1];
 	name += ".ini";
-
+	int quizresultgrab = getQuizResults(name);
+	switch(quizresultgrab){
+	case 0:
+		break;
+	case 1:
+		cout<<"<h1>Question file syntax error.</h1>"<<endl<<"<p>Please alert your instructor, there is an error inside the [quiz] section.</p>"<<endl;
+		break;
+	case 2:
+		cout<<"<h1>Question file syntax error.</h1>"<<endl<<"<p>Please alert your instructor, there is an error inside the [question] section.</p>"<<endl;
+		break;
+	case 3:
+		cout<<"<h1>Question file syntax error.</h1>"<<endl<<"<p>Please alert your instructor, there is an extra [bracketed] set in the question file.</p>"<<endl;
+		break;
+	case 4:
+		cout<<"<h1>Bad quiz ID.</h1>"<<endl<<"<p>The quiz ID you have supplied is wrong. Please ask your instructor for a new one.</p>"<<endl;
+		break;
+	}
+	if(quizresultgrab != 0){
+		cout<<"</body>"<<endl<<"</html>"<<endl;
+		return 2;
+	}
+	return 0;
 }
