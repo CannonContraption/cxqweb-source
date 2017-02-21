a0c04fbb790b3b91ee126df1a9cb0fb0888a2a44
Jim Read <jimmydean886@hotmail.com>
2017-02-21 15:55:49 -0500

    Set the file to append to the score file
    Allows for multiple attemtps, also allows for multiple students

--- cxqweb-source-gradeit.cc
+++ cxqweb-source-gradeit.cc
@@ -10,14 +10,14 @@ int main(){
 	parsekvstrings(true);
 	fstream scorefile;
 	string sfname = "quizresults/"+kvpairs[0].value+".txt";
-	scorefile.open(sfname, fstream::out);
+	scorefile.open(sfname, fstream::app);
 	string sfstring;
 	string studentemail = kvpairs[3].value;
-	scorefile<<kvpairs[2].value<<","<<kvpairs[1].value<<" ";
+	scorefile<<kvpairs[2].value<<","<<kvpairs[1].value<<"	";
 	cout<<"KVCOUNT: "<<kvpaircount<<endl;
 	for(int i = 4; i<kvpaircount; i++){//4 components before questions, then dynamically created question answer list
 		scorefile<<kvpairs[i].value;
-		if(i%4 == 0) scorefile<<" ";
+		if(i+3%4 == 0) scorefile<<" ";
 	}
 	scorefile<<endl;
 	return 0;
