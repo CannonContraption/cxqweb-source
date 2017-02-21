2548ca18dafa4a353e0b8ac1c1716ebf0f3d0ca1
Jim Read <jimmydean886@hotmail.com>
2017-02-21 15:51:13 -0500

    switched to plain getquery output
    also now displays query count

--- cxqweb-source-gradeit.cc
+++ cxqweb-source-gradeit.cc
@@ -6,7 +6,7 @@ using namespace std;
 using namespace cgim;
 
 int main(){
-	cout<<"Content-type: text/html\r\n\r\n"<<endl;
+	cout<<"Content-type: text/plain\r\n\r\n"<<endl;
 	parsekvstrings(true);
 	fstream scorefile;
 	string sfname = "quizresults/"+kvpairs[0].value+".txt";
@@ -14,6 +14,7 @@ int main(){
 	string sfstring;
 	string studentemail = kvpairs[3].value;
 	scorefile<<kvpairs[2].value<<","<<kvpairs[1].value<<" ";
+	cout<<"KVCOUNT: "<<kvcount<<endl;
 	for(int i = 4; i<kvpaircount; i++){//4 components before questions, then dynamically created question answer list
 		scorefile>>kvpairs[i].value;
 		if(i%4 == 0) scorefile<<" ";
