b1d6d6d29eec934134437a57e0cadd0cd36ea9fd
Jim Read <jimmydean886@hotmail.com>
2017-02-21 12:23:02 -0500

    Added query parsing software, form updates
    The forms now correctly update based on the cppcgimanager code.
    (github.com/CanonContraption/cppcgimanager)

--- cxqweb-source-main.cc
+++ cxqweb-source-main.cc
@@ -105,8 +105,8 @@ int format_php_quiz(string quizname){
 	if(master_quiz.questionlist.size()<1) return 1;
 	cout<<"<h2>"<<master_quiz.name<<"</h2>"<<endl;
 	cout<<"<h3>"<<master_quiz.questionlist.size()<<" questions."<<endl;
-	cout<<"<form id=quiz action=submitquiz.cgi>"<<endl;
-	cout<<"<table><tr><td>Name</td><td>E-Mail</td></tr><tr><td><input type=text class=textfield></input></td><td><input type=text class=textfield></input></td></tr></table>"<<endl;
+	cout<<"<form id=quiz action=submitquiz.cgi method=GET>"<<endl;
+	cout<<"<table><tr><td>First Name</td><td>Last Name</td><td>E-Mail</td></tr><tr><td><input type=text name=fname class=textfield></input></td><td><input type=text name=lname class=textfield></input></td><td><input class=textfield type=text name=email></input></td></tr></table>"<<endl;
 	int optionlistsize;
 	int questionlistsize = master_quiz.questionlist.size();
 	question currentquestion;
